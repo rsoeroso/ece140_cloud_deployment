de6ec66928ec6ce31e10ea5f05e0486b69db1df1
Rasya Soeroso <rasyasoeroso@Rasyas-MBP-2.attlocal.net>
2022-04-19 23:40:15 -0700

    landing page assignment2

--- web/src/server.py
+++ web/src/server.py
@@ -1,31 +1,13 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.renderers import render_to_response
-
-import mysql.connector as mysql
-import os
-
-db_user = os.environ['MYSQL_USER']
-db_pass = os.environ['MYSQL_PASSWORD']
-db_name = os.environ['MYSQL_DATABASE']
-db_host = os.environ['MYSQL_HOST']
+from pyramid.response import FileResponse
 
 def get_home(req):
-  # Connect to the database and retrieve the users
-  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
-  cursor = db.cursor()
-  cursor.execute("select first_name, last_name, email from Users;")
-  records = cursor.fetchall()
-  db.close()
-
-  return render_to_response('templates/home.html', {'users': records}, request=req)
+    return FileResponse('templates/home.html')
 
 ''' Route Configurations '''
 if __name__ == '__main__':
   config = Configurator()
-
-  config.include('pyramid_jinja2')
-  config.add_jinja2_renderer('.html')
 
   config.add_route('get_home', '/')
   config.add_view(get_home, route_name='get_home')
